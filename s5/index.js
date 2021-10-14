7b10923e52237b9740cdab99a0d2b76ac52d2073
vodaihuu <vodaihuu42@gmail.com>
2021-10-14 13:26:12 +0700

    add

---
+++ s5/index.js
@@ -0,0 +1,106 @@
+// 1.1
+// const getRandomArbitrary = (min, max) => 
+// {
+//     return Math.random() * (max - min) + min;
+// };
+
+// for (let i = 0; i < 10; i += 1)
+// {
+//     console.log(`Random number ${i}: ${getRandomArbitrary(0,1)}`);
+// }
+// 1.2
+
+
+// 2.1
+// var menu = ['to', 'be', 'that', 'of', 'elon', 'to', 'this', 'now', 'back', 'cool', 'hey', 'love', 'of', 'life', 'that', 'rain', 'summer', 'color', 'now', 'of', 'hat', 'late', 'sorry', 'my', 'team']
+// var counts = {};
+
+// for (var i = 0; i < menu.length; i++) {
+//   var num = menu[i];
+//   counts[num] = counts[num] ? counts[num] + 1 : 1;
+// }
+
+// console.log(
+//   ' to:'+counts['to']+'\n',
+//   'be:'+counts['be']+'\n',
+//   'that:'+counts['that']+'\n', 
+//   'of:'+counts['of']+'\n',
+//   'elon:'+counts['elon']+'\n',
+//   'this:'+counts['this']+'\n',
+//   'now:'+counts['now']+'\n',
+//   'back:'+counts['back']+'\n',
+//   'cool:'+counts['cool']+'\n',
+//   'hey:'+counts['hey']+'\n',
+//   'love:'+counts['love']+'\n',
+//   'life:'+counts['life']+'\n',
+//   'rain:' +counts['rain']+'\n',
+//   'summer:'+counts['summer']+'\n',
+//   'color:'+counts['color']+'\n',
+//   'hat:'+counts['hat']+'\n',
+//   'late:'+counts['late']+'\n',
+//   'sorry:'+counts['sorry']+'\n',
+//   'my:' +counts['my']+'\n',
+//   'team:'+counts['team']);
+
+//   2.2
+//   const inventory = [
+//     {
+//         name: 'HP Envy 13aq',
+//         price: 21000,
+//         brand: 'HP',
+//         quantity: 5,
+//     },
+//     {
+//         name: 'Dell XPS 9370',
+//         price: 30000,
+//         brand: 'Dell',
+//         quantity: 1,
+//     },
+//     {
+//         name: 'Dell Inspiron 3567',
+//         price: 9300,
+//         brand: 'Dell',
+//         quantity: 12,
+//     },
+//     {
+//         name: 'Dell Latitude E5450',
+//         price: 8600,
+//         brand: 'Dell',
+//         quantity: 2,
+//     },
+
+//     {
+//         name: 'Asus Zenbook',
+//         brand: 'Asus',
+//         price: 20000,
+//         quantity: 4,
+//     },
+//     {
+//         name: 'HP Pavilion',
+//         brand: 'HP',
+//         price: 14000,
+//         quantity: 7,
+//     },]
+    
+// const inventoryByBrand = 
+// {
+//     'HP':[],
+//     'Dell':[],
+//     'Asus':[],
+// }
+// for (let i = 0; i < inventory.length; i++) 
+// {
+//    const inv = inventory[i];
+//    console.log(inv);
+//    const brand = inv.brand;
+//    console.log(brand);
+//    inventoryByBrand[brand].push(inv);
+// }
+// console.log(inventoryByBrand['HP']);
+// console.log(inventoryByBrand['Dell']);
+// console.log(inventoryByBrand['Asus']);
+// 2.3
+
+
+
+
--- testt/index.js
+++ testt/index.js
@@ -70,19 +70,20 @@
 //     }
 // }    
 // bài 5.a
-day   = prompt ('nhập ngày')
-month = prompt ('nhập tháng')
-year  = prompt ('nhập năm')
-ngay = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
-thang = [1,2,3,4,5,6,7,8,9,10,11,12]
-while(day!= ngay && month!=thang)   
-{
-        alert('ngày: ' +day+ ' tháng: ' +month+ ' năm: ' +year+ ' không hợp lệ ')
-}
-     if (!(day!= ngay && month!=thang))
-{
-        alert('ngày: ' +day+ ' tháng: ' +month+ ' năm: ' +year+ ' hợp lệ ')
-}
-
+// day   = prompt ('nhập ngày')
+// month = prompt ('nhập tháng')
+// year  = prompt ('nhập năm')
+// ngay = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+// thang = [1,2,3,4,5,6,7,8,9,10,11,12]
+// while(day!= ngay && month!=thang)   
+// {
+//         alert('ngày: ' +day+ ' tháng: ' +month+ ' năm: ' +year+ ' không hợp lệ ')
+// }
+//      if (!(day!= ngay && month!=thang))
+// {
+//         alert('ngày: ' +day+ ' tháng: ' +month+ ' năm: ' +year+ ' hợp lệ ')
+// }
+// 5b
+// không bit lam 
 
 
